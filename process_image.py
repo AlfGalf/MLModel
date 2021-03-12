75fb84691aa5a5256ad4d6be3a74c67b7623950d
Alfie Richards <alfierchrds@gmail.com>
2021-03-12 19:33:25 +0000

    Initial attempt

--- process_image.py
+++ process_image.py
@@ -1,14 +1,40 @@
 
 import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+from fastai import *
+from fastai.vision.all import *
 import numpy as np
 
-model = torch.load("model")
-model.eval()
+from m_utils import itoa
 
-size = 150
+mod_path = "/Users/alfierichards/Documents/GitRepos/IPPythonWebServer/MLModel"
+
+# learn = synth_learner(path=mod_path)
+# learn = learn.load("model.pkl")
+
+learner = load_learner(mod_path + '/learn_export.pkl')
+
+def label_func(f):
+    return f[0]
+
+data_transforms = transforms.Compose([
+    transforms.Resize(size),
+    transforms.ToTensor()
+])
+
+def image_loader(loader, image):
+    image = loader(image).float()
+    image = torch.tensor(image, requires_grad=True)
+    image = image.unsqueeze(0)
+    return image
+
 
 def predict_image(image):
     print(image)
-    out = model(image)
+
+    out = learner.predict(image)
     print(out)
-    return out
+    return 'A'
+
