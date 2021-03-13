3944c0fab96b51ebf5beba5a304d49a34bc603af
Alfie Richards <alfierchrds@gmail.com>
2021-03-13 10:49:32 +0000

    Clean-up

--- process_image.py
+++ process_image.py
@@ -6,33 +6,21 @@ from torch.autograd import Variable
 from fastai import *
 from fastai.vision.all import *
 import numpy as np
-import pathlib
-temp = pathlib.PosixPath
-pathlib.PosixPath = pathlib.WindowsPath
 from m_utils import *
+from PIL import Image
 
-mod_path = 'C:\\Users\\Joseph\\Desktop\\Uni\\Y2\\IP\\IPPythonServer\\MLModel'
+mod_path = '/Users/alfierichards/Documents/GitRepos/MLModel/model.pth'
 
-# learn = synth_learner(path=mod_path)
-# learn = learn.load("model.pkl")
+learner = load_learner(mod_path)
 
-learner = load_learner(mod_path + '\\export.pkl')
-
-#data_transforms = transforms.Compose([
-#    transforms.Resize(size),
-##    transforms.ToTensor()
-#])
-
-def image_loader(loader, image):
-    image = loader(image).float()
-    image = torch.tensor(image, requires_grad=True)
-    image = image.unsqueeze(0)
-    return image
-
+def to_byte_arr(image):
+    byteIO = io.BytesIO()
+    img.save(byteIO, format='PNG')
+    byteArr = byteIO.getvalue()
+    return byteArr
 
 def predict_image(image):
 
-    out = learner.predict(image)
-    print(out)
+    out = learner.predict(to_byte_arr(image))
     return out[0]
 
